f883ab5488c07e33f861e62daa5193df9135d9d8
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-07-03 19:49:12 -0400

    deploy

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const cors =  require('cors')
 const jwt = require('jsonwebtoken')
 const  bcrypt  =  require("bcrypt");
+const PORT = process.env.PORT || 5000
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -17,9 +19,15 @@ app.use('/home', require('./controllers/home'))
 
 app.use('/store', require('./controllers/store'))
 
+if (process.env.NODE_ENV === "production") {
+  
+    app.use(express.static(path.join(__dirname, "public")));
+  }
+  
+  console.log(__dirname);
+  console.log(path.join(__dirname, "public"));
 
 
-
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
     console.log('listening...')
 })
