657afa86d35fcc0b0a6f126572ca4b7f9cb00777
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2022-07-04 00:03:58 -0400

    db

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const  bcrypt  =  require("bcrypt");
 const PORT = process.env.PORT || 5000
 const path = require("path");
 require('dotenv').config()
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'))
@@ -19,13 +20,13 @@ app.use('/home', require('./controllers/home'))
 
 app.use('/store', require('./controllers/store'))
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   
-    app.use(express.static(path.join(__dirname, "public")));
-  }
+//     app.use(express.static(path.join(__dirname, "public")));
+//   }
   
-  console.log(__dirname);
-  console.log(path.join(__dirname, "public"));
+//   console.log(__dirname);
+//   console.log(path.join(__dirname, "public"));
 
 
 app.listen(PORT, ()=>{
