24411a1fd53d9e2c698c7e1576bd9e08dac63523
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2023-01-10 14:48:33 -0500

    cors

--- postgres.js
+++ postgres.js
@@ -1,14 +1,8 @@
 const Client = require('pg').Client
 require('dotenv').config()
-const dbConfig = {
-	connectionString: 'postgresql://localhost:5432/clients',
-}
+const dbConfig =  process.env.HEROKU_POSTGRESQL_GOLD_URL
 
-if(process.env.HEROKU_POSTGRESQL_GOLD_URL){
-	dbConfig.ssl = { rejectUnauthorized: false }
-	dbConfig.connectionString = process.env.HEROKU_POSTGRESQL_GOLD_URL
 
-}
 
 const client = new Client(dbConfig)
 
