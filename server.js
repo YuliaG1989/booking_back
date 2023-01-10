8d7bb8e8b9309989a9031f49eb2685ca2412323a
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2023-01-10 14:44:00 -0500

    cors

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 5000
 const path = require("path");
 require('dotenv').config()
 
-app.use(cors({
-    origin: 'https://thegreatcat.herokuapp.com/'
-}));
+app.use('*', cors());
 app.use(express.json());
 app.use(express.static('public'))
 
