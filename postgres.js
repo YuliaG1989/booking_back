aeb6551c33527fb05f4466476692f889854ae920
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2023-01-11 10:12:57 -0500

    change port

--- postgres.js
+++ postgres.js
@@ -1,6 +1,12 @@
 const Client = require('pg').Client
 require('dotenv').config()
-const dbConfig = {connectionString : process.env.HEROKU_POSTGRESQL_GOLD_URL
+const dbConfig = {
+	connectionString: 'postgresql://localhost:5432/clients',
+}
+
+if(process.env.HEROKU_POSTGRESQL_GOLD_URL){
+	dbConfig.ssl = { rejectUnauthorized: false }
+	dbConfig.connectionString = process.env.HEROKU_POSTGRESQL_GOLD_URL
 
 }
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors =  require('cors')
 const jwt = require('jsonwebtoken')
 const  bcrypt  =  require("bcrypt");
 require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const PORT = 5432
 const path = require("path");
 
 
