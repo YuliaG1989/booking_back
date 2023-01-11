f9ce73e2886f3189a81dffa90c847ca7e84a04fb
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2023-01-11 10:29:20 -0500

    change port

--- postgres.js
+++ postgres.js
@@ -3,12 +3,7 @@ require('dotenv').config()
 
 
 const dbConfig = {
-	connectionString: 'postgresql://localhost:5432/clients',
-}
-
-if(process.env.HEROKU_POSTGRESQL_GOLD_URL){
-	dbConfig.ssl = { rejectUnauthorized: false }
-	dbConfig.connectionString = 'postgres://neigafaikehsyn:39318d3b33e716454d167ffa61047c732ae549b4c973324a29c13adcb5819134@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d802dltkbkr8pv'
+	connectionString: 'postgres://neigafaikehsyn:39318d3b33e716454d167ffa61047c732ae549b4c973324a29c13adcb5819134@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d802dltkbkr8pv'
 }
 
 const client = new Client(dbConfig)
