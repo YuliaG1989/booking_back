6d318632ef269492c65a4a0a17d13edd9d35b32c
Yulia Glushenko <yuliaglushenko@Yulias-MacBook-Air.local>
2023-01-12 15:23:43 -0500

    smth

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors =  require('cors')
 const jwt = require('jsonwebtoken')
 const  bcrypt  =  require("bcrypt");
 require('dotenv').config()
-const PORT = 5432
+const PORT = process.env.PORT
 const path = require("path");
 
 
